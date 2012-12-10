0da79ae7eb51b6535715121270fcc1f20baae0c8
Adrian <adtf1990@gmail.es>
2012-12-10 12:08:05 +0100

    Añadido PPrueba

---
+++ PPrueba/src/serpis/ed/Prueba.java
@@ -0,0 +1,14 @@
+package serpis.ed;
+
+import java.util.Date;
+
+public class Prueba {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hora "+ new Date());
+
+	}
+
+}
